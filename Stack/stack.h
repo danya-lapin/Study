6aa27789407b9aa701a5deb064f8c87eeb48b704
Daniil <danyalapin@yandex.ru>
2019-02-18 23:17:37 +0300

    test

--- Stack/stack.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-typedef struct stack stack;
-stack *create_stack(int value);
-stack *push(stack *head, int value);
-void delete_stack(stack *head);
-stack *pop(stack *head);
-int get_head_data(stack *head);
-stack *find_value(stack *head, int value);
--- Stack/test.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "stack.h"
-
-void create_stack_test(int value);
-void delete_stack_test(stack *head);
-void find_value_test(stack *head);
-void push_test(stack *head, int value);
-void pop_test(stack *head);
